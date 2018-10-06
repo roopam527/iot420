6f6a066ff49aba190386bf92c4126107874c5cf9
roopam527 <roopamg777@gmail.com>
2018-10-06 14:25:31 +0530

    message

--- temperature.js
+++ temperature.js
@@ -18,18 +18,23 @@
 
 // sendTemperature()
 
+
+const api ="$2a$10$dN8pi4KOJG6fSt5kv8dZduc3T5gVlhQxTmkNK6qCV1VQNhhKbtNfG";
 const temperature = ()=>{
     
-    fetch("https://obscure-shore-41041.herokuapp.com/temperature/$2a$10$apKzU4dirjjo.qrIxQiDcOxCbIIQ7bVnvGLHZ1hpjE7GSul1RqGOq/"+document.getElementById("room_temperature").value);
+    fetch("https://obscure-shore-41041.herokuapp.com/temperature/"+api+"/"+document.getElementById("room_temperature").value)
+    .then(()=>{
+        console.log("Hello")
+    })
     console.log("room temperature")
 }
 
 const body_temperature = ()=>{
-    fetch("https://obscure-shore-41041.herokuapp.com/body_temperature/$2a$10$apKzU4dirjjo.qrIxQiDcOxCbIIQ7bVnvGLHZ1hpjE7GSul1RqGOq/"+document.getElementById("body_temperature").value);
+    fetch("https://obscure-shore-41041.herokuapp.com/body_temperature/"+api+"/"+document.getElementById("body_temperature").value);
     console.log("body temperature")
 }
 
 const pulse =()=>{
-    fetch("https://obscure-shore-41041.herokuapp.com/pulse/$2a$10$apKzU4dirjjo.qrIxQiDcOxCbIIQ7bVnvGLHZ1hpjE7GSul1RqGOq/"+document.getElementById("pulse").value);
+    fetch("https://obscure-shore-41041.herokuapp.com/pulse/"+api+"/"+document.getElementById("pulse").value);
 console.log("Pulse")
 }
